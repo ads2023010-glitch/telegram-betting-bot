ad85a044c7c961d02568dd500efeec7e72957dd6
ads2023010-glitch <ads2023010@gmail.com>
2026-03-11 22:51:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,3 +21,28 @@ def main():
 
 if __name__ == "__main__":
     main()
+async def scan(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    cote1 = 2.0
+    cote2 = 1.65
+
+    mise1 = 7
+    mise2 = round((mise1 * cote1) / cote2, 2)
+
+    gain_si_1 = round(mise1 * cote1 - mise1 - mise2, 2)
+    gain_si_x2 = round(mise2 * cote2 - mise1 - mise2, 2)
+
+    message = f"""
+Match trouvé ⚽
+
+Cote 1 : {cote1}
+Cote X2 : {cote2}
+
+Mise 1 : {mise1}€
+Mise X2 : {mise2}€
+
+Gain si 1 gagne : {gain_si_1}€
+Gain si X2 gagne : {gain_si_x2}€
+"""
+
+    await update.message.reply_text(message)
